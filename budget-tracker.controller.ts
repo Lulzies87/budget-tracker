6352f2c6b192b61aab61c4389988a84e168ff243
Lilach E <lilachelayza@gmail.com>
2023-11-24 09:24:57 +0200

    add amount validation

--- budget-tracker.controller.ts
+++ budget-tracker.controller.ts
@@ -4,13 +4,14 @@ import { displayTransactions, loadFormFields } from "./budget-tracker.view.js";
 export function activateForm() {
   loadFormFields();
 
-  document.forms.namedItem("log-transaction")?.addEventListener("submit", (e) => {
-
+  document.forms
+    .namedItem("log-transaction")
+    ?.addEventListener("submit", (e) => {
       const formData = new FormData(e.target as HTMLFormElement);
 
       const transactionObject = {
         type: parseData(formData.get("typeInput"), "type"),
-        amount: parseData(Number(formData.get("amountInput")), "amount"),
+        amount: parseAmount(Number(formData.get("amountInput")), "amount"),
         category: parseData(formData.get("categoryInput"), "category"),
         date: parseData(formData.get("dateInput"), "date"),
       };
@@ -28,7 +29,16 @@ function parseData(input: any | null, key: string) {
   }
 }
 
+function parseAmount(input: any, key: string) {
+  if (isNaN(input)) {
+    throw new Error(`${key} must be a number!`);
+  } else if (input <= 0) {
+    throw new Error(`${key} must be a positive number!`);
+  }
+  return input;
+}
+
 function logTransaction(transaction: Transaction) {
-    transactions.push(transaction);
-    localStorage.setItem('transactions', JSON.stringify(transactions));
+  transactions.push(transaction);
+  localStorage.setItem("transactions", JSON.stringify(transactions));
 }
--- dist/budget-tracker.controller.js
+++ dist/budget-tracker.controller.js
@@ -2,11 +2,13 @@ import { transactions } from "./budget-tracker.model.js";
 import { displayTransactions, loadFormFields } from "./budget-tracker.view.js";
 export function activateForm() {
     loadFormFields();
-    document.forms.namedItem("log-transaction")?.addEventListener("submit", (e) => {
+    document.forms
+        .namedItem("log-transaction")
+        ?.addEventListener("submit", (e) => {
         const formData = new FormData(e.target);
         const transactionObject = {
             type: parseData(formData.get("typeInput"), "type"),
-            amount: parseData(Number(formData.get("amountInput")), "amount"),
+            amount: parseAmount(Number(formData.get("amountInput")), "amount"),
             category: parseData(formData.get("categoryInput"), "category"),
             date: parseData(formData.get("dateInput"), "date"),
         };
@@ -22,7 +24,16 @@ function parseData(input, key) {
         return input;
     }
 }
+function parseAmount(input, key) {
+    if (isNaN(input)) {
+        throw new Error(`${key} must be a number!`);
+    }
+    else if (input <= 0) {
+        throw new Error(`${key} must be a positive number!`);
+    }
+    return input;
+}
 function logTransaction(transaction) {
     transactions.push(transaction);
-    localStorage.setItem('transactions', JSON.stringify(transactions));
+    localStorage.setItem("transactions", JSON.stringify(transactions));
 }
